2e92edaa499df616234ba615efcc35e2da600e8e
DAL225 <255dal@gmail.com>
2026-04-10 08:15:39 -0600

    Clase InicioSesión.java

---
+++ src/main/java/com/mycompany/restaurante/equipo/a/InicioSesiónController.java
@@ -0,0 +1,36 @@
+package com.mycompany.restaurante.equipo.a;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+/**
+ * FXML Controller class
+ *
+ * @author aburt
+ */
+public class InicioSesiónController implements Initializable {
+
+    @FXML
+    private TextField nombreUsuario;
+    @FXML
+    private PasswordField contraseña;
+    @FXML
+    private Button botonIniciarSesion;
+  
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+  
+    @FXML
+    private void IniciarSesion(ActionEvent event) {
+        nombreUsuario.getText();
+        botonIniciarSesion.getText();
+    }
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.mycompany.restaurante.equipo.a {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.base;
 
     opens com.mycompany.restaurante.equipo.a to javafx.fxml;
     exports com.mycompany.restaurante.equipo.a;
