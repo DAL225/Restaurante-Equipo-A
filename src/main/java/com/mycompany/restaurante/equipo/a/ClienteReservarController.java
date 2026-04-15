fbde1ac4eb6a904d90a0db23356d90bdc7ada372
DAL225 <255dal@gmail.com>
2026-04-15 07:35:06 -0600

    Reservar Funcional, no conectato a BD

--- src/main/java/com/mycompany/restaurante/equipo/a/ClienteReservarController.java
+++ src/main/java/com/mycompany/restaurante/equipo/a/ClienteReservarController.java
@@ -1,13 +1,14 @@
 
 package com.mycompany.restaurante.equipo.a;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.SpinnerValueFactory;
-
 import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Spinner;
@@ -25,13 +26,9 @@ public class ClienteReservarController implements Initializable {
     @FXML
     private DatePicker EntradaFechaR;
     @FXML
-    private TextField SalidaEstadoMesa;
-    @FXML
     private TextField EntradaNombreR;
     @FXML
     private Button BotonSalir;
-    @FXML
-    private Spinner<Integer> EntradaNumMesaR;
     @FXML
     private Spinner<Integer> EntradaNumPersonasR;
     @FXML
@@ -41,12 +38,8 @@ public class ClienteReservarController implements Initializable {
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        int MesaMin = 1;
-        int MesaMax = 10; //Acá habrá que traer directamente el número máximo de mesas existentes
         int PersonasMin = 1; //Acá habría que cambiarlo según la mesa
-        int PersonasMax = 10; //Acá habría que cambiarlo según la mesa
-        SpinnerValueFactory<Integer> valueFactoryMesa = new SpinnerValueFactory.IntegerSpinnerValueFactory(MesaMin, MesaMax, 1);
-        EntradaNumMesaR.setValueFactory(valueFactoryMesa);
+        int PersonasMax = 100; //Acá habría que cambiarlo según la mesa
         SpinnerValueFactory<Integer> valueFactoryPersonas = new SpinnerValueFactory.IntegerSpinnerValueFactory(PersonasMin, PersonasMax, 1);
         EntradaNumPersonasR.setValueFactory(valueFactoryPersonas);
         SpinnerValueFactory<Integer> valueFactoryMin = new SpinnerValueFactory.IntegerSpinnerValueFactory(1, 60, 1);
@@ -57,9 +50,15 @@ public class ClienteReservarController implements Initializable {
 
     @FXML
     private void ReservarMesa(ActionEvent event) {
+        System.out.println(EntradaNumPersonasR.getValue());
+        System.out.println(EntradaFechaR.getValue());
+        System.out.println(EntradaMinR.getValue());
+        System.out.println(EntradaHoraR.getValue());
+        System.out.println(EntradaNombreR.getText());
     }
 
     @FXML
-    private void Salir(ActionEvent event) {
+    private void Salir(ActionEvent event) throws IOException {
+        App.setRoot("InicioSesión");
     }
 }
