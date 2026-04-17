757f77f619953fe59e65c0d1fe72f3466163dae8
DAL225 <255dal@gmail.com>
2026-04-17 08:12:15 -0600

    Confirmación de datos de Reserva

--- src/main/java/com/mycompany/restaurante/equipo/a/ClienteReservarController.java
+++ src/main/java/com/mycompany/restaurante/equipo/a/ClienteReservarController.java
@@ -3,16 +3,21 @@ package com.mycompany.restaurante.equipo.a;
 
 import java.io.IOException;
 import java.net.URL;
+import java.time.LocalDate;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.SpinnerValueFactory;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Spinner;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 /**
  * FXML Controller class
  *
@@ -42,23 +47,57 @@ public class ClienteReservarController implements Initializable {
         int PersonasMax = 100; //Acá habría que cambiarlo según la mesa
         SpinnerValueFactory<Integer> valueFactoryPersonas = new SpinnerValueFactory.IntegerSpinnerValueFactory(PersonasMin, PersonasMax, 1);
         EntradaNumPersonasR.setValueFactory(valueFactoryPersonas);
-        SpinnerValueFactory<Integer> valueFactoryMin = new SpinnerValueFactory.IntegerSpinnerValueFactory(1, 60, 1);
+        SpinnerValueFactory<Integer> valueFactoryMin = new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 59, 1);
         EntradaMinR.setValueFactory(valueFactoryMin);
-        SpinnerValueFactory<Integer> valueFactoryHora = new SpinnerValueFactory.IntegerSpinnerValueFactory(1, 24, 1);
+        SpinnerValueFactory<Integer> valueFactoryHora = new SpinnerValueFactory.IntegerSpinnerValueFactory(8, 19, 1);
         EntradaHoraR.setValueFactory(valueFactoryHora);
     }
 
     @FXML
     private void ReservarMesa(ActionEvent event) {
-        System.out.println(EntradaNumPersonasR.getValue());
-        System.out.println(EntradaFechaR.getValue());
-        System.out.println(EntradaMinR.getValue());
-        System.out.println(EntradaHoraR.getValue());
-        System.out.println(EntradaNombreR.getText());
+        if (this.verificarDatos(EntradaFechaR.getValue())){
+            int NumPersonasR = EntradaNumPersonasR.getValue();
+            LocalDate FechaR = EntradaFechaR.getValue();
+            int MinR = EntradaMinR.getValue();
+            int HoraR = EntradaHoraR.getValue();
+            String NombreR = EntradaNombreR.getText();
+            String confirmación = "Reservar la mesa el "+FechaR+" a la "+HoraR+" con "+MinR+"?";
+            Alert confirmar = new Alert(AlertType.CONFIRMATION, confirmación);
+            confirmar.showAndWait();
+            if (confirmar.getResult() == ButtonType.OK){
+                //acá el método para darle los datos a la base de datos pa que de mesa
+                this.mostrarAlerta("Mesa reservada exitosamente: Mesa "/*+ mesa.getNumero*/);
+            } else {
+                this.mostrarAlerta("Hubo un problema al reservar la mesa");
+            }
+        }
+        EntradaFechaR.setValue(null);
+        EntradaNumPersonasR.getValueFactory().setValue(1);
+        EntradaMinR.getValueFactory().setValue(0);
+        EntradaHoraR.getValueFactory().setValue(8);
+        EntradaNombreR.setText(null);
     }
 
     @FXML
     private void Salir(ActionEvent event) throws IOException {
         App.setRoot("InicioSesión");
+    }
+    
+    private boolean verificarDatos(LocalDate fecha) {
+        LocalDate fechaActual = LocalDate.now();
+        if (fecha.isAfter(fechaActual)){
+            return true;
+        } else {
+            this.mostrarAlerta("La fecha de reservación debe ser posterior a la fecha de reserva");
+            return false;
+        }
+    }
+    
+    private void mostrarAlerta(String mensaje) {
+        Alert alert = new Alert(Alert.AlertType.WARNING);
+        alert.setTitle("AVISO");
+        alert.setHeaderText(null);
+        alert.setContentText(mensaje);
+        alert.showAndWait();
     }
 }
