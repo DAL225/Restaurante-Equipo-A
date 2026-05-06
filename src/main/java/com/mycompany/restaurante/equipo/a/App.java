95a23ca5ecbef1219129aca2189ea5c1c6f2abc9
osmarAnt <dontstarvero678@gmai.com>
2026-05-05 22:05:12 -0600

    añadir app al commmit

--- src/main/java/com/mycompany/restaurante/equipo/a/App.java
+++ src/main/java/com/mycompany/restaurante/equipo/a/App.java
@@ -17,7 +17,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("Gerente"));
+        scene = new Scene(loadFXML("InicioSesion"));
         stage.setScene(scene);
         stage.show();
     }
